3cf23a8906c5fd92267cd4e5dcb95e40331bd3f0
ItzEmoji <99118329+TheEmojiGamer@users.noreply.github.com>
2023-06-04 09:30:58 +0200

    Add files via upload

---
+++ bapeclient.bat
@@ -0,0 +1,8 @@
+echo off
+mode 55, 9
+title Starting Vape...
+echo Cracked by Bape team
+echo Vape Cracked Private
+ping localhost -n 5.5 >nul
+title Vape Client (Cracked by Bape team)
+java --add-opens java.base/java.lang=ALL-UNNAMED -jar vape-loader.jar
